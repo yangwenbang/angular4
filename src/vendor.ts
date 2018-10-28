6cae7e4b0c56dfc8497174fde6aa42b738915df6
yangwenbang <1471275556@qq.com>
2018-10-28 13:51:24 +0800

    xxx

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,5 +1,5 @@
 // Angular 2
-// import '@angular/platform-browser-dynamic';
+import '@angular/platform-browser-dynamic';
 import '@angular/platform-browser';
 import '@angular/core';
 import '@angular/common';
